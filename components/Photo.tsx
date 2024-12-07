7d98ff100b5bddfca3ded73e40cf3c741fdaf8b8
junayed-hassan <junayedhassan102045@gmail>
2024-12-08 00:45:50 +0600

    updata

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -23,7 +23,7 @@ function Photo() {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/phot.png"
+            src="/assets/photo.png"
             priority
             quality={100}
             fill
